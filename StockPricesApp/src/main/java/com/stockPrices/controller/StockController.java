c4e94f76833cc6ae508ac2b96ddc85d69b1d11c5
Nisha Gadhave <nishagadhave26@gmail.com>
2023-05-11 16:05:36 +0530

    committed

--- StockPricesApp/src/main/java/com/stockPrices/controller/StockController.java
+++ StockPricesApp/src/main/java/com/stockPrices/controller/StockController.java
@@ -3,9 +3,9 @@ package com.stockPrices.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +14,13 @@ import com.stockPrices.service.StockService;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(value = "/stockApp")
+//@RequestMapping(value = "/stockApp")
 public class StockController {
 
 	@Autowired
 	StockService stockService;
 
-	@MessageMapping("/stocks")
+	@GetMapping("/stocks")
 	@SendTo("/topic/stocks") 
 	//@GetMapping(value="/stocks ", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<StockEntity> getAllStocks() {
